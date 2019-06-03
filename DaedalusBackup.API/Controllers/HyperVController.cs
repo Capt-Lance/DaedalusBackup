6b6c64c127985dba595d27da3963abca011b4d1b
Capt-Lance <bryonnush@gmail.com>
2019-06-02 22:15:21 -0500

    Added initial API work

---
+++ DaedalusBackup.API/Controllers/HyperVController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackupManagement.Domain;
+using BackupManagement.Infrastructure.HyperV.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaedalusBackup.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HyperVController : ControllerBase
+    {
+        private readonly HyperVVirtualMachineRepository _hyperVRepo;
+        public HyperVController(HyperVVirtualMachineRepository hyperVRepo)
+        {
+            _hyperVRepo = hyperVRepo;
+        }
+        [HttpGet("virtualmachaines")]
+        public IActionResult GetVMs()
+        {
+            IEnumerable<VirtualMachine> vms = _hyperVRepo.GetAll();
+            return Ok(vms);
+        }
+
+    }
+}
