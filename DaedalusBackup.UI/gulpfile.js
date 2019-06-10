8fab617cb7ed868d675577883b3288b1ea0ad349
Capt-Lance <bryonnush@gmail.com>
2019-06-09 22:39:40 -0500

    rearranged nav, switched to FA icons

--- DaedalusBackup.UI/gulpfile.js
+++ DaedalusBackup.UI/gulpfile.js
@@ -1,4 +1,5 @@
-﻿var gulp = require('gulp');
+﻿/// <binding BeforeBuild='scss' ProjectOpened='move-fa' />
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('scss', function () {
@@ -8,3 +9,13 @@ gulp.task('scss', function () {
         }))
         .pipe(gulp.dest('wwwroot/css'))
 });
+
+gulp.task('move-fa', function () {
+    return gulp.src(['node_modules/@fortawesome/fontawesome-free/css/all.min.css')
+        .pipe(gulp.dest('wwwroot/css'))
+})
+
+gulp.task('move-webfonts', function () {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('wwwroot/webfonts'))
+})
