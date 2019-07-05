7f6361610ba2751025d87b8ccd0e784fd0d71e87
Capt-Lance <bryonnush@gmail.com>
2019-07-04 20:59:28 -0500

    fixed IncrementalBackupTest for updated test repos

--- BackupManagement.UnitTests/Backups/IncrementalBackupTest.cs
+++ BackupManagement.UnitTests/Backups/IncrementalBackupTest.cs
@@ -13,7 +13,7 @@ namespace BackupManagement.UnitTests.Backups
         [Fact]
         public async Task OriginalIncrementCreated()
         {
-            byte[] newData = new byte[8923];
+            byte[] newData = new byte[789];
             Random rndm = new Random();
             rndm.NextBytes(newData);
             var readStream = new MemoryStream(newData);
@@ -27,7 +27,7 @@ namespace BackupManagement.UnitTests.Backups
         [Fact]
         public async Task DataNotCorrupted()
         {
-            int dataLength = 8923;
+            int dataLength = 789;
             byte[] newData = new byte[dataLength];
             Random rndm = new Random();
             rndm.NextBytes(newData);
@@ -36,25 +36,13 @@ namespace BackupManagement.UnitTests.Backups
             int incrementSize = 536870912; //512 MB
             string path = "";
             IncrementalBackup backup = await IncrementalBackup.CreateFromStreamAsync(readStream, streamFactory, path, incrementSize);
-            bool isSame = backup.OriginalIncrement.Chunks.Count == 1;
-            if (isSame)
-            {
-                var sourceStream = new MemoryStream(newData);
-                int chunkIndex = 0;
-                while (sourceStream.Position < sourceStream.Length)
-                {
-                    Chunk chunk = backup.OriginalIncrement.Chunks.First(x => x.Id == chunkIndex);
-                    Stream targetChunkStream = streamFactory.Open(chunk, path);
-                    int comparisonSize = dataLength < incrementSize ? dataLength : incrementSize;
-                    byte[] sourceChunkData = new byte[comparisonSize];
-                    byte[] targetChunkData = new byte[comparisonSize];
-                    await sourceStream.ReadAsync(sourceChunkData);
-                    await targetChunkStream.ReadAsync(targetChunkData);
-                    isSame = sourceChunkData.SequenceEqual(targetChunkData) && isSame;
-                    chunkIndex++;
-                }
-            }
-            Assert.True(isSame, "Written data does not match original data");
+
+            string incrementName = backup.OriginalIncrement.Chunks[0].Hash;
+            Stream targetStream = streamFactory.Open(incrementName);
+            MemoryStream targetMs = new MemoryStream();
+            targetStream.CopyTo(targetMs);
+            byte[] targetData = targetMs.ToArray();
+            Assert.True(newData.SequenceEqual(targetData), "Written data does not match original data");
         }
     }
 }
