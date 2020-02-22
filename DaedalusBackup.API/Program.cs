7ceffd821342a4f04d7acaa088465f2cecd4b83b
Capt-Lance <bryonnush@gmail.com>
2020-02-21 20:29:35 -0600

    messing around with branches

--- DaedalusBackup.API/Program.cs
+++ DaedalusBackup.API/Program.cs
@@ -20,5 +20,6 @@ namespace DaedalusBackup.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
